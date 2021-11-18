807f1bdb1faa55b610ce57f3f2989e3300250f37
hanpeng <hanpeng@css.com.cn>
2021-11-18 15:02:34 +0800

    代码回顾与注释添加

--- src/main/java/com/gui/demo/thingInJava/concurrency/parallelStream/Summing2.java
+++ src/main/java/com/gui/demo/thingInJava/concurrency/parallelStream/Summing2.java
@@ -29,12 +29,14 @@ public class Summing2 {
     public static void main(String[] args) {
         System.out.println(CHECK);
         long[] la = new long[SZ + 1];
-        //根据第二个参数generator来对数组的数据进行处理并将其替换原数据，如果generator为null，抛出异常
+        //根据第二个参数generator来对生成一个数组长度大小的有序队列，并将值其数组值替换，如果generator为null，抛出异常
         Arrays.parallelSetAll(la, i -> i);
+
         Summing.timeTest("Array Stream Sum", CHECK, () -> Arrays.stream(la).sum());
         Summing.timeTest("Parallel", CHECK, () -> Arrays.stream(la).parallel().sum());
         Summing.timeTest("Basic Sum", CHECK, () -> basicSum(la));
         Summing.timeTest("ParallelPrefix",CHECK,()->{
+            //这个方法会将数组的前几位的值求和并加上第i位的值赋值给la[i]；la[la.length - 1]的值就是求和
             Arrays.parallelPrefix(la, Long::sum);
             return la[la.length - 1];
         });
